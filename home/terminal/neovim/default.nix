1925a0f405b671d648000f548e80484d484334da
J0wsper <bramschuijff03@gmail.com>
2025-05-21 13:55:01 -0700

    configuring neovim with nix

---
+++ home/terminal/neovim/default.nix
@@ -0,0 +1,44 @@
+{ config, inputs, pkgs }:
+
+{
+	programs.neovim = {
+		enable = true;
+		defaultEditor = true;
+		extraLuaConfig = builtins.readFile ./init.lua;
+		extraLuaPackages = luaPkgs: with luaPkgs; [];
+		extraPackages = [
+			pkgs.ripgrep
+			pkgs.nil
+			pkgs.nixd
+			pkgs.lua-language-server
+			pkgs.python3Packages.pyright
+			pkgs.rust-analyzer
+			pkgs.fish-lsp
+		];
+		plugins = with pkgs.vimPlugins; [
+			# Important backend
+			nvim-lspconfig
+			nvim-cmp
+			cmp-nvim-lsp
+			cmp_luasnip
+			luasnip
+
+			# QoL plugins
+			leap-nvim
+			telescope-nvim
+			comment-nvim
+			dropbar-nvim
+			which-key-nvim
+			gitsigns-nvim
+			noice-nvim
+			trouble-nvim
+			lualine-nvim
+		];
+	};
+	xdg.configFile = {
+		"nvim/snippets".source = ./snippets;
+		"nvim/after/ftplugins".source = ./after/ftplugin;
+		"nvim/lua".source = ./lua;
+		"nvim/ftplugin".source = ./ftplugin;
+	};
+}
