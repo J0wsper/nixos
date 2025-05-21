81ad0ff065efeda5f40516744f4b6a30b4d228ce
J0wsper <bramschuijff03@gmail.com>
2025-05-21 16:17:15 -0700

    adding gruvbox colors to tmux

--- home/terminal/apps/tmux.nix
+++ home/terminal/apps/tmux.nix
@@ -4,8 +4,21 @@
 	programs.tmux = {
 		enable = true;
 		clock24 = true;
-		# extraConfig = ''
-		# 	set-option -g default-shell /run/current-system/sw/bin/fish
-		# '';
+		plugins = with pkgs; [
+			tmuxPlugins.better-mouse-mode
+			tmuxPlugins.vim-tmux-navigator
+			tmuxPlugins.gruvbox	
+		];
+
+		# https://www.reddit.com/r/NixOS/comments/12mdyzv/need_help_installing_tmux_plugins_through/
+		extraConfig = ''
+			set -g mouse on
+
+			bind -n M-h select-pane -L
+			bind -n M-j select-pane -D
+			bind -n M-k select-pane -U
+			bind -n M-l select-pane -R
+		'';
+		
 	};
 }
