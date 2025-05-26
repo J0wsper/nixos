dcf789753396b2b92db76ad9275f32cf5a992bd4
J0wsper <bramschuijff03@gmail.com>
2025-05-25 20:12:20 -0700

    setting up dropbar keybindings

--- home/terminal/neovim/init.lua
+++ home/terminal/neovim/init.lua
@@ -27,12 +27,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 })
 
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', {desc = 'Move focus to the left window'})
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', {desc = 'Move focus to the left window'})
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', {desc = 'Move focus to the left window'})
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', {desc = 'Move focus to the left window'})
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>', {desc = 'Move focus to the right window'})
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>', {desc = 'Move focus to the bottom window'})
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>', {desc = 'Move focus to the top window'})
 
-require("telescope")
+-- require("telescope")
+-- require("dropbar")
 --  require("lsp")
+require ("misc")
 --  require("snippets")
 --  require("theme")
 --  require("clipboard")
---
+++ home/terminal/neovim/lua/misc.lua
@@ -0,0 +1,12 @@
+-- Dropbar configuration
+local dropbar_api = require('dropbar.api')
+vim.keymap.set('n', '<leader>;', dropbar_api.pick, { desc = "Pick symbols in winbar"})
+vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = "Go to start of current context"})
+vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = "Select next context"})
+
+-- Telescope configuration
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>sb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
