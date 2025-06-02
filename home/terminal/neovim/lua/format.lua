e4ccaea33dec7ff645a64a64f2fbd0496e93b5ff
J0wsper <bramschuijff03@gmail.com>
2025-06-02 11:49:14 -0600

    adding diagnostic tools to neovim

--- home/terminal/neovim/lua/format.lua
+++ home/terminal/neovim/lua/format.lua
@@ -1,18 +1,19 @@
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    -- Conform will run multiple formatters sequentially
-    python = { "ruff" },
-    -- You can customize some of the format options for the filetype (:help conform.format)
-    rust = { "rustfmt", lsp_format = "fallback" },
-    -- Conform will run the first available formatter
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		-- Conform will run multiple formatters sequentially
+		python = { "ruff" },
+		-- You can customize some of the format options for the filetype (:help conform.format)
+		rust = { "rustfmt", lsp_format = "fallback" },
+		-- Conform will run the first available formatter
+		javascript = { "prettierd", "prettier", stop_after_first = true },
+		nix = { "nixfmt" },
+	},
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
+	pattern = "*",
+	callback = function(args)
+		require("conform").format({ bufnr = args.buf })
+	end,
 })
--- home/terminal/neovim/lua/misc.lua
+++ home/terminal/neovim/lua/misc.lua
@@ -5,23 +5,27 @@ vim.o.smarttab = false
 vim.o.number = true
 vim.o.undofile = true
 
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, { desc = "open diagnostic floating window" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "prev diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "next diagnostic" })
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, { desc = "open diagnostic buffer" })
+
 -- Setting up assorted configs
 require("Comment").setup()
-require("which-key").setup {}
+require("which-key").setup({})
 require("noice").setup()
-require("trouble").setup {}
-require('leap').set_default_mappings()
+require("trouble").setup({})
+require("leap").set_default_mappings()
 
 -- Dropbar configuration
-local dropbar_api = require('dropbar.api')
-vim.keymap.set('n', '<leader>;', dropbar_api.pick, { desc = "Pick symbols in winbar"})
-vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = "Go to start of current context"})
-vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = "Select next context"})
+local dropbar_api = require("dropbar.api")
+vim.keymap.set("n", "<leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
 
 -- Telescope configuration
-local telescope_builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>sf', telescope_builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>sg', telescope_builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>sb', telescope_builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>sh', telescope_builtin.help_tags, { desc = 'Telescope help tags' })
-
+local telescope_builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>sf", telescope_builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>sg", telescope_builtin.live_grep, { desc = "Telescope live grep" })
+vim.keymap.set("n", "<leader>sb", telescope_builtin.buffers, { desc = "Telescope buffers" })
+vim.keymap.set("n", "<leader>sh", telescope_builtin.help_tags, { desc = "Telescope help tags" })
