9a83bca2a8e4b6f53f2fd048a6765024887c1351
J0wsper <bramschuijff03@gmail.com>
2025-06-02 11:52:57 -0600

    changing nixfmt to nixfmt-classic

--- home/terminal/neovim/default.nix
+++ home/terminal/neovim/default.nix
@@ -1,58 +1,58 @@
 { config, inputs, pkgs, lib, ... }:
 
 {
-	programs.neovim = {
-		enable = true;
-		defaultEditor = true;
-		extraLuaConfig = builtins.readFile ./init.lua;
-		extraLuaPackages = luaPkgs: with luaPkgs; [];
-		extraPackages = [
-			pkgs.ripgrep
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    extraLuaConfig = builtins.readFile ./init.lua;
+    extraLuaPackages = luaPkgs: with luaPkgs; [ ];
+    extraPackages = [
+      pkgs.ripgrep
 
-			# Language servers
-			pkgs.nil
-			pkgs.nixd
-			pkgs.lua-language-server
-			pkgs.rust-analyzer
-			pkgs.fish-lsp
+      # Language servers
+      pkgs.nil
+      pkgs.nixd
+      pkgs.lua-language-server
+      pkgs.rust-analyzer
+      pkgs.fish-lsp
 
-            # Formatters
-            pkgs.ruff
-            pkgs.rustfmt
-            pkgs.stylua
-            pkgs.nixfmt
-		];
-		plugins = with pkgs.vimPlugins; [
-			# Important backend
-			nvim-lspconfig
-			nvim-cmp
-			cmp-nvim-lsp
-			cmp_luasnip
-			luasnip
-            conform-nvim
+      # Formatters
+      pkgs.ruff
+      pkgs.rustfmt
+      pkgs.stylua
+      pkgs.nixfmt-classic
+    ];
+    plugins = with pkgs.vimPlugins; [
+      # Important backend
+      nvim-lspconfig
+      nvim-cmp
+      cmp-nvim-lsp
+      cmp_luasnip
+      luasnip
+      conform-nvim
 
-			# QoL plugins
-			leap-nvim
-			telescope-nvim
-			comment-nvim
-			dropbar-nvim
-			which-key-nvim
-			gitsigns-nvim
-			noice-nvim
-			trouble-nvim
-			lualine-nvim
-            nvim-treesitter.withAllGrammars
-            nvim-web-devicons
-            leap-nvim
+      # QoL plugins
+      leap-nvim
+      telescope-nvim
+      comment-nvim
+      dropbar-nvim
+      which-key-nvim
+      gitsigns-nvim
+      noice-nvim
+      trouble-nvim
+      lualine-nvim
+      nvim-treesitter.withAllGrammars
+      nvim-web-devicons
+      leap-nvim
 
-            # Themes
-            catppuccin-nvim
-		];
-	};
-	xdg.configFile = {
-		# "nvim/snippets".source = ./snippets;
-		# "nvim/after/ftplugins".source = ./after/ftplugin;
-		"nvim/lua".source = ./lua;
-		# "nvim/ftplugin".source = ./ftplugin;
-	};
+      # Themes
+      catppuccin-nvim
+    ];
+  };
+  xdg.configFile = {
+    # "nvim/snippets".source = ./snippets;
+    # "nvim/after/ftplugins".source = ./after/ftplugin;
+    "nvim/lua".source = ./lua;
+    # "nvim/ftplugin".source = ./ftplugin;
+  };
 }
