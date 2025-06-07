439e9d53b0c6777cf643a5d1f9869ba6c78a2c9b
J0wsper <bramschuijff03@gmail.com>
2025-06-07 10:23:01 -0600

    setting conceal level to 0 for markdown files

--- home/terminal/neovim/ftplugin/markdown.lua
+++ home/terminal/neovim/ftplugin/markdown.lua
@@ -1,7 +1,7 @@
 vim.cmd("setlocal spell")
 vim.cmd("setlocal fo+=at")
 vim.cmd("setlocal shiftwidth=2")
-vim.cmd("setlocal conceallevel=2")
+vim.cmd("setlocal conceallevel=0")
 vim.api.nvim_create_autocmd("BufWritePre", {
 	callback = function()
 		vim.cmd("setlocal textwidth=100")
